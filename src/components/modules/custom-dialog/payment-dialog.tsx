64e07f1302238dc3297cca5db5d17b548ce93f26
Jerry <junhyuk.dev@gmail.com>
2025-10-15 13:25:41 +0900

    fix: calendar disabled add

--- src/components/modules/custom-dialog/payment-dialog.tsx
+++ src/components/modules/custom-dialog/payment-dialog.tsx
@@ -86,6 +86,7 @@ export default function PaymentDialog({
   const handleDialogOpen = () => {
     setFormData(getInitialFormData());
     setPaymentDialogOpen(true);
+    setCalendarOpen(false);
   };
 
   return (
@@ -130,6 +131,7 @@ export default function PaymentDialog({
                         setCalendarOpen(false);
                       }
                     }}
+                    disabled={(date) => date > new Date()}
                   />
                 </PopoverContent>
               </Popover>
